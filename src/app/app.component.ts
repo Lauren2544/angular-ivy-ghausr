fa16d702f2abe78afbbbc26e043c64efb9ca3586
Lauren2544 <lauren.rowe2544@gmail.com>
2022-03-18 23:45:14 -0400

    pressing the mail icon and the stop receiving button messes up the message counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,27 @@ import { interval } from 'rxjs';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  //the current state of the button
   is_recieving: string;
   message_counter: number = 0;
+  //toggle function
   toggle(starting: string) {
     const obs$ = interval(300);
     obs$.subscribe((d) => {
       if (starting == 'start') {
+        //prints out the message counter -- works
         this.message_counter = d;
       } else if (starting == 'home') {
+        //should set the counter to zero but this is obviously not how that is  done -- fails
         this.message_counter = 0;
       } else {
+        //should set the counter to the current last state again doesn't work -- fails
         this.message_counter = this.message_counter;
       }
     });
   }
 }
+// //another attempt
 // export class AppComponent implements OnInit {
 //   // name = 'Angular ' + VERSION.major;
 //   is_recieving: string;
