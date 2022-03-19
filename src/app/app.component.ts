51930592c9dd8e1975dc50463f2e5aca338c5055
Lauren2544 <lauren.rowe2544@gmail.com>
2022-03-18 23:17:20 -0400

    stop and restart not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,30 @@ import { interval } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  // name = 'Angular ' + VERSION.major;
+export class AppComponent {
   is_recieving: string;
-  message_counter: number;
-  ngOnInit(): void {
-    // if (this.is_recieving) {
+  message_counter: number = 0;
+  toggle(starting: string) {
     const obs$ = interval(300);
     obs$.subscribe((d) => {
-      this.message_counter = d;
+      if (starting == 'start') {
+        this.message_counter = d;
+      } else if (starting == 'home') {
+        this.message_counter = 0;
+      } else {
+        this.message_counter = this.message_counter;
+      }
     });
-    // }
   }
 }
+// export class AppComponent implements OnInit {
+//   // name = 'Angular ' + VERSION.major;
+//   is_recieving: string;
+//   message_counter: number;
+//   ngOnInit(): void {
+//     const obs$ = interval(300);
+//     obs$.subscribe((d) => {
+//       this.message_counter = d;
+//     });
+//   }
+// }
